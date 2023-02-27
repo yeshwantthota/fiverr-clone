0a1343e2adebcf4e37f08bc6728d5b2023a6dd05
Yeshwant Thota <thotayeshwant@gmail.com>
2023-02-27 23:24:50 +0530

    half home page done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import CatCard from "../../components/catCard/CatCard";
+import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import { cards } from "../../data";
 import "./Home.scss";
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
+    </div>
+  );
 };
 
 export default Home;
